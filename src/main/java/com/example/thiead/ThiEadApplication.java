365c8fdb97894ed726162fa765b888b218691fbb
HongQuang <nguyenhongquang2002@gmail.com>
2022-07-30 14:43:18 +0700

    day len da

--- src/main/java/com/example/thiead/ThiEadApplication.java
+++ src/main/java/com/example/thiead/ThiEadApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThiEadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThiEadApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThiEadApplication.class, args);
+    }
 
 }
